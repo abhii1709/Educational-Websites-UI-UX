bc2689d6c80186ec29c4c6c22d2ba32a74666717
abhii1709 <choudharyabhishek876@gmail.com>
2024-07-24 17:20:20 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const header = document.querySelector("header");
+const hamburger = document.querySelector("#hamburger");
+hamburger.addEventListener("click", () => {
+  header.classList.toggle("clicked");
+});
+const headerLinks = document.querySelectorAll("header a");
+headerLinks.forEach((headerLink) => {
+  headerLink.addEventListener("click", () => {
+    header.classList.remove("clicked");
+  });
+});
+
+if (window.pageYOffset > 30) {
+  header.style.boxShadow = " 0 0 .2rem black";
+} else {
+  header.style.boxShadow = "none";
+}
+
+window.onscroll = () => {
+  if (window.pageYOffset > 30) {
+    header.style.boxShadow = " 0 0 .2rem black";
+  } else {
+    header.style.boxShadow = "none";
+  }
+};
